4b21ff30d2d1bdd5f360e7afb54b682d2ac57fc2
kowshik-gk <kowshik462003@gmail.com>
2024-03-08 11:53:33 +0530

    Reffresh2

---
+++ Evolution-two(1)/Empty/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolution_two_1_
+{
+    public static class Data
+    {
+       
+        public static int TotalExpense; 
+        public static int id=6;
+
+        public static Dictionary<int, string> D = new Dictionary<int, string>();
+
+        public static List<CExpense> ExpenseData = new List<CExpense>();
+
+        public static HashSet<string> CatogoryCollection = new HashSet<string>();
+
+
+
+
+        public static Dictionary<int, string> CatogoryId= new Dictionary<int, string>();
+        public static Dictionary<DateTime, List<Catogory>> CatogoryToMonthLimit = new Dictionary<DateTime, List<Catogory>>();
+        public static Dictionary<DateTime, List<Catogory>> CurrentCatogoryToMonthLimit = new Dictionary<DateTime, List<Catogory>>();
+
+
+
+
+        public static Dictionary<DateTime, int> Limit = new Dictionary<DateTime, int>();
+        public static Dictionary<DateTime, int> CurrentLimit = new Dictionary<DateTime, int>(); 
+       
+
+      
+
+    }
+}
